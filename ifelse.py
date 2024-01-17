1fcd7da33976e68992f10088f6a66ce3e0084f5e
rak5hi <rakshirakshi9945@gmail.com>
2024-01-17 18:35:51 +0530

    intialized

---
+++ ifelse.py
@@ -0,0 +1,15 @@
+def age(age=24):
+
+    if age > 18:
+        print("you're too old")
+    else:
+        print("you're a child")
+            
+def Age(age=24):
+    
+    #ternary operator
+    print("you're too old") if age>18 else print("you're a child")
+    
+
+    
+
