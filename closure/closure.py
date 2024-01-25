02f31ed3db65312104a7e8aad776a70d6c967763
rak5hi <rakshirakshi9945@gmail.com>
2024-01-25 17:20:39 +0530

    update

--- closure/closure.py
+++ closure/closure.py
@@ -21,13 +21,13 @@ def non_closure(num):
 
 _base10= non_closure(34)
 #base10()
-
+####################################################
 
 def power(x):
     def inner(y):
         return y**x
     return inner
 
-
 cube=power(3)
 print(cube(5))
+####################################################
