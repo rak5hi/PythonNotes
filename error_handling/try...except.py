53f90498e0f1261a88af6545b300ab1db1d1950a
rak5hi <rakshirakshi9945@gmail.com>
2024-01-30 22:37:34 +0530

    update

--- error_handling/try...except.py
+++ error_handling/try...except.py
@@ -11,4 +11,42 @@ def do_math():
         print('something went wrong...', e)
         
         
-do_math()
+#do_math()
+
+def user_input():
+    
+    user_input: str = input('ENTER: ')
+    
+    try:
+        num = float(user_input)
+        print(num)
+    except Exception as e:
+        print('Exception:', e)
+    else: 
+        print('Successfully executed code!')
+    finally: 
+        print('This will always run')
+        #try:
+             #.....
+             
+#user_input()
+
+def _raise():
+    is_connect: bool = False
+    
+    def connection():
+        if not is_connect:
+            #raise Exception("no internet!")
+            raise ConnectionError("no internet!")
+        else:
+            print('connect to internet!')
+            
+    try:
+        connection()
+    except ConnectionError:
+        print('There is no connection!')
+    except Exception as e:
+        print(e)
+        
+_raise()
+        
